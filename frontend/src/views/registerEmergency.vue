4a4028e29f1ed3fc10250ce241ff552849c8aad0
Carlos Vera Ramírez <carlos.vera.r@usach.cl>
2023-10-22 18:25:02 -0300

    Carlos Vera - Create Submit Function in RegisterEmergency View

--- frontend/src/views/registerEmergency.vue
+++ frontend/src/views/registerEmergency.vue
@@ -2,9 +2,10 @@
 
 import TopBar from '../components/TopBar.vue';
 import SideBar from "../components/SideBar.vue";
+import axios from 'axios';
 </script>
-<script>
 
+<script>
 export default {
   data() {
     return {
@@ -16,14 +17,24 @@ export default {
     };
   },
   methods: {
-    submitForm() {
-      // Enviar formulario
-      console.log('Nombre de Emergencia:', this.titulo);
-      console.log('Tipo de emergencia:', this.tipo);
-      console.log('Ubicación:', this.ubicacion);
-      console.log('Descripción y requisitos:', this.descripcion);
-      console.log('Equipamiento necesario:', this.equipamiento_necesario);
+    async submitForm() {
+      /*
+      try {
+        const response = await axios.post('/emergencia', {
+          id: null, // Puedes enviar null o dejar este campo en blanco si el backend lo maneja automáticamente
+          idInstitucion: null, // Puedes enviar null o dejar este campo en blanco si el backend lo maneja automáticamente
+          tipo: this.tipo,
+          ubicacion: this.ubicacion,
+          equipamiento_necesario: this.equipamiento_necesario,
+          titulo: this.titulo,
+          descripcion: this.descripcion,
+        });
 
+        // Aquí puedes manejar la respuesta del servidor si es necesario
+        console.log('Respuesta del servidor:', response.data);
+      } catch (error) {
+        console.error('Error al enviar el formulario:', error);
+      }*/
     }
   }
 }
@@ -31,41 +42,43 @@ export default {
 
 
 <template>
-    <div>
-      <TopBar/>
-      <SideBar />
-      <div class="view">
-        <div class="content">
-          <h1>Crear Emergencia</h1>
-          <p>A continuación rellene la siguiente información</p>
-          <div class="form-container">
-            
-            <div class="card" style="margin-top: 20px;">
-              <form @submit.prevent="submitForm">
-                <label for="nombre-emergencia">Nombre de Emergencia:</label>
-                <input type="text" id="nombre-emergencia" v-model="titulo" required>
+  <div>
+    <TopBar />
+    <SideBar />
+    <div class="view">
+      <div class="content">
+        <h1>Crear Emergencia</h1>
+        <p>A continuación rellene la siguiente información</p>
+        <div class="form-container">
 
-                <label for="tipo-emergencia">Tipo de emergencia:</label>
-                <input type="text" id="tipo-emergencia" v-model="tipo" required>
+          <div class="card" style="margin-top: 20px;">
+            <form @submit.prevent="submitForm">
+              <label for="nombre-emergencia">Nombre de Emergencia:</label>
+              <input type="text" id="nombre-emergencia" v-model="titulo" required>
 
-                <label for="ubicacion">Ubicación:</label>
-                <input type="text" id="ubicacion" v-model="ubicacion" required>
+              <label for="tipo-emergencia">Tipo de emergencia:</label>
+              <input type="text" id="tipo-emergencia" v-model="tipo" required>
 
-    
-                <label for="descripcion">Descripción y requisitos:</label>
-                <textarea id="descripcion" v-model="descripcion" placeholder="Detalle su emergencia e ingrese una lista de requisitos"></textarea>
+              <label for="ubicacion">Ubicación:</label>
+              <input type="text" id="ubicacion" v-model="ubicacion" required>
 
-                <label for="equipamiento">Equipamiento necesario:</label>
-                <textarea id="equipamiento" v-model="equipamiento_necesario" placeholder="Detalle el equipamiento necesario"></textarea>
-    
-                <button type="submit">Registrar Emergencia</button>
-              </form>
-            </div>
+
+              <label for="descripcion">Descripción y requisitos:</label>
+              <textarea id="descripcion" v-model="descripcion"
+                placeholder="Detalle su emergencia e ingrese una lista de requisitos"></textarea>
+
+              <label for="equipamiento">Equipamiento necesario:</label>
+              <textarea id="equipamiento" v-model="equipamiento_necesario"
+                placeholder="Detalle el equipamiento necesario"></textarea>
+
+              <button type="submit">Registrar Emergencia</button>
+            </form>
           </div>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
 
 <style scoped>
 .view {
@@ -121,7 +134,9 @@ form {
   max-width: 300px;
 }
 
-input, textarea, select, button {
+input,
+textarea,
+select,
+button {
   margin-bottom: 10px;
-}
-</style>
+}</style>
